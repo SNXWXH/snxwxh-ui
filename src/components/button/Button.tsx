fa44542087b6091eb5d77017d4753fdc8b8ec72d
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2025-07-23 09:45:08 +0900

    feat: 버튼 컴포넌트 구현 및 스토리북 적용 (#8)
    * chore: 스토리북 예제 파일 삭제
    
    * chore: 버튼 및 스타일 관련 파일 삭제
    
    * feat: button 컴포넌트 구현
    
    * feat: button 전체 컴포넌트 조합 적용
    
    * fix: button 컴포넌트 상태 클래스 수정
    
    * fix: button 컴포넌트 isDisable prop로 변경 및 버튼 컴포넌트 props 타입 추가
    
    * feat: button 컴포넌트 스토리북 구현

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,56 @@
+import type { ComponentProps, ReactNode } from 'react';
+
+type ButtonProps = {
+  variant?: 'primary' | 'secondary' | 'outline' | 'ghost' | 'destrctive';
+  size?: 'mini' | 'small' | 'regular' | 'large';
+  isDisable?: boolean;
+  children?: ReactNode;
+};
+
+export const Button = ({
+  variant = 'primary',
+  size = 'regular',
+  isDisable = false,
+  children,
+  ...props
+}: ComponentProps<'button'> & ButtonProps) => {
+  const variantClass = {
+    primary: 'bg-neutral-900 text-white',
+    secondary: 'bg-neutral-100  text-black',
+    outline: 'bg-neutral-100 border border-neutral-200 shadow-md text-black',
+    ghost: 'bg-transparent text-black hover:bg-neutral-100',
+    destrctive: 'bg-danger-600 text-white',
+  };
+
+  const sizeClass = {
+    mini: 'h-6 px-2 py-0.5 text-xs rounded-sm',
+    small: 'h-8 px-3 py-1 text-sm rounded-lg',
+    regular: 'h-9 px-4 py-1.5 text-base rounded-lg',
+    large: 'h-11 px-5 py-2 text-lg rounded-lg',
+  };
+
+  const stateClass = {
+    default:
+      'hover:opacity-90 active:scale-95 focus:outline-none focus:ring-2 focus:ring-neutral-200 cursor-pointer',
+    disabled: 'opacity-50 cursor-not-allowed',
+  };
+
+  const state = isDisable ? 'disabled' : 'default';
+
+  return (
+    <>
+      <button
+        className={`
+        flex items-center justify-center transition-all duration-200 font-medium
+        ${variantClass[variant]}
+        ${sizeClass[size]}
+        ${stateClass[state]}
+      `}
+        disabled={isDisable}
+        {...props}
+      >
+        {children}
+      </button>
+    </>
+  );
+};
